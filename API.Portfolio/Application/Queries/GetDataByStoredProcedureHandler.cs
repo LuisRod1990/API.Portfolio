0d313b9be5b9895ae0dd070e03dacabd03c7334d
LuisRod1990 <luisrodj1990@gmail.com>
2026-02-07 07:04:34 -0600

    Se ajusta a UTC

--- API.Portfolio/Application/Queries/GetDataByStoredProcedureHandler.cs
+++ API.Portfolio/Application/Queries/GetDataByStoredProcedureHandler.cs
@@ -32,24 +32,13 @@ namespace PortfolioApi.Application.Queries
         public async Task<IEnumerable<dynamic>> Handle(GetDataByStoredProcedureQuery request, CancellationToken ct)
         {
             using var connection = _context.CreateConnection();
-            try
-            {
-                // Validar que la función esté permitida
-                if (!_allowedFunctions.Contains(request.StoredProcedure))
-                    throw new ArgumentException($"Función no permitida: {request.StoredProcedure}");
-
-                // Construir el SQL dinámicamente con esquema dbo
-                var sql = $"SELECT * FROM dbo.{request.StoredProcedure}(@UsuarioId);";
-
-                var result = await connection.QueryAsync(sql, new { UsuarioId = request.UsuarioId });
-
-                return result;
-            }
-            catch (Exception ex)
-            {
+            // Validar que la función esté permitida
+            if (!_allowedFunctions.Contains(request.StoredProcedure))
                 throw new ArgumentException($"Función no permitida: {request.StoredProcedure}");
-            }
-            
+            // Construir el PostgreSQL dinámicamente con esquema dbo
+            var sql = $"SELECT * FROM dbo.{request.StoredProcedure}(@UsuarioId);";
+            var result = await connection.QueryAsync(sql, new { UsuarioId = request.UsuarioId });
+            return result;
         }
     }
 }
