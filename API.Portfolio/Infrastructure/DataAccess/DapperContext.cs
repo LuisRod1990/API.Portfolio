e3ae9620f11cb17867a61edbb30d9cb5d66a7434
LuisRod1990 <luisrodj1990@gmail.com>
2026-02-07 06:02:11 -0600

    Se agrega cors domai Duck DNS registrado

--- API.Portfolio/Infrastructure/DataAccess/DapperContext.cs
+++ API.Portfolio/Infrastructure/DataAccess/DapperContext.cs
@@ -15,8 +15,7 @@ namespace PortfolioApi.Infrastructure.DataAccess
         // sql
         //public IDbConnection CreateConnection() => new SqlConnection(Environment.GetEnvironmentVariable("CONN"));
         public IDbConnection CreateConnection()
-            //=> new NpgsqlConnection(Environment.GetEnvironmentVariable("CONN"));
-            => new NpgsqlConnection("Host=db-cv.ctoeigs0euz0.us-east-2.rds.amazonaws.com;Port=5432;Database=db-cv;Username=postgres;Password=MxN1990*-;Ssl Mode = Require; Trust Server Certificate=true");
+            => new NpgsqlConnection(Environment.GetEnvironmentVariable("CONN"));
     }
     
 }
