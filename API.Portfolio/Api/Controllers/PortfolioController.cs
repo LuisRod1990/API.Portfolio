ad7f6cf009dbca950e12a7ef7ad721bb6a2a0b96
LuisRod1990 <luisrodj1990@gmail.com>
2026-02-08 07:48:06 -0600

    Actualización parámetros de controller

--- API.Portfolio/Api/Controllers/PortfolioController.cs
+++ API.Portfolio/Api/Controllers/PortfolioController.cs
@@ -23,18 +23,26 @@ namespace PortfolioApi.Api.Controllers
         {
             return Ok("Success...");
         }
-        
-        [HttpGet("{sp}/{usuarioId}")]
-        public async Task<IActionResult> GetData(string sp, int usuarioId)
+
+        [HttpGet]
+        public async Task<IActionResult> GetData([FromQuery] string sp, [FromQuery] int usuarioId)
         {
-            var allowed = new[] { "get_contact", "get_aptitudes", "get_experiencia", "get_skills", "get_totalexperiencia", "get_formacion" };
+            var allowed = new[]
+            {
+                "get_contact",
+                "get_aptitudes",
+                "get_experiencia",
+                "get_skills",
+                "get_totalexperiencia",
+                "get_formacion"
+            };
+
             if (!allowed.Contains(sp))
                 return BadRequest("Stored procedure no permitida");
 
             var result = await _mediator.Send(new GetDataByStoredProcedureQuery(sp, usuarioId));
             return Ok(result);
         }
-
 
     }
 }
